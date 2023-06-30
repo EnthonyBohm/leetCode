160764871493ec224cc558207a955adf6f65ffce
Enthony Gabriel Bohm <114608439+EnthonyBohm@users.noreply.github.com>
2023-06-30 18:11:59 -0300

    Add files via upload

--- leet746.c
+++ leet746.c
@@ -4,37 +4,69 @@ int minCostClimbingStairs(int* cost, int costSize);
 int encontraMenor(int* vetor, int tamVetor, int posAtual);
 
 int main(){
+    int valorFinal;
+    // int teste1[3] = {10, 15, 20};
+    // minCostClimbingStairs(teste1, 3);
 
-    int teste1[3] = {10, 15, 20};
-    minCostClimbingStairs(teste1, 3);
+    // int teste1[10] = {1,100,1,1,1,100,1,1,100,1};
+    // minCostClimbingStairs(teste1, 10);
 
+    int teste1[4] = {0, 1, 2, 2};
+    valorFinal = minCostClimbingStairs(teste1, 4);
+
+    printf("\n Valor Final: %d", valorFinal);
     return 0;
 }
 
 
 int minCostClimbingStairs(int* cost, int costSize){
-    int i, j, finalValue = 0, tempValue1, tempValue2;
+    int finalValue = 0, tempValue1, tempValue2, funcResult, funcResult2;
+    int i, j;
     
-    /*
-    Testar o primeiro valor, ja somando ele, preciso de variável nova, de soma_provisória
-    Testar esse valor com o primeiro e o segundo e ver qual deles é o menor ou, se o 
-    Se o próxima valor for
-    */
-    
-    for(i = 0, j=i+1; i < costSize;){
-        tempValue1= 0;
-        tempValue2 = 0;
-        
+    for(i = 0, j= 1; i < costSize;){
         if(i == 0){
-            tempValue1 += cost[i];
-            tempValue2 += cost[j];
-            i = encontraMenor(cost, costSize, i);
-            tempValue1 += cost[i];
-            j = encontraMenor(cost, costSize, j);
-            tempValue2 += cost[j];
+            tempValue1 = cost[i];
+            tempValue2 = cost[j];
 
-            printf("1:%d & 2:%d", tempValue1, tempValue2);
+            funcResult = encontraMenor(cost, costSize, i);
+            funcResult2 = encontraMenor(cost, costSize, j);
+            
+            if(funcResult == -1){
+                i+=1;
+                finalValue = tempValue1;
+                break;
+            }
+            else
+                tempValue1 += cost[funcResult];
+                
+                
+            if(funcResult2 == -1){
+                i += 2;
+                finalValue = tempValue2;
+                break;
+            }
+            else
+                tempValue2 += cost[funcResult2];
+                
+
+            if(tempValue1 < tempValue2){
+                i = funcResult;
+                finalValue += cost[0];
+            }
+            else{
+                i = funcResult2;
+                finalValue += cost[1];
+            }
+
         }
+        else{
+            finalValue += cost[i];
+            funcResult = encontraMenor(cost, costSize, i);
+            if(funcResult == -1)
+                break;
+            i += funcResult;
+        }
+    }
         return finalValue;
     }
     
@@ -42,11 +74,11 @@ int minCostClimbingStairs(int* cost, int costSize){
 int encontraMenor(int* vetor, int tamVetor, int posAtual){
     int first, second;
     
-    if((posAtual +1 )< tamVetor){
-        return posAtual +1;
+    if((posAtual +1 )>= tamVetor){
+        return -1;
     }
-    if((posAtual +2) < tamVetor ){
-        return posAtual +2;
+    if((posAtual +2) >= tamVetor ){
+        return -1;
     }
 
     first = vetor[posAtual+1];

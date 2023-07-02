cf1ca8ff67e005c62a61fe0184606004cd7092fb
Enthony Gabriel Bohm <114608439+EnthonyBohm@users.noreply.github.com>
2023-07-02 11:58:13 -0300

    EstadoFinal
    Achei um vídeo explicando a lógica,de fazer de trás pra frente e atualizar os valores no vetor, o vídeo era em Python então não olhei a solução e tentei implementar, o resultado ficou muito legal, por ter ficado tudo em pouquíssimas linhas

--- leet746.c
+++ leet746.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
 int minCostClimbingStairs(int* cost, int costSize);
-int encontraMenor(int* vetor, int tamVetor, int posAtual);
+/*
+    A função próximo pulo define qual vai ser o pulo escolhido, de acordo com o fim do vetor, ou com o menor valor encontrado, retorno o pulo desejado
+    Quando o pulo não for para a parte final, é escolhido o menor custo, e em seguida, o valor naquela posição do vetor muda para um novo valor, o novo custo
+    Este custo retrata a distância até chegar ao final do vetor
+*/
+int nextJump(int* vector, int maxPosition,int currentPosition);
 
 int main(){
     int valorFinal;
-    // int teste1[3] = {10, 15, 20};
-    // minCostClimbingStairs(teste1, 3);
+    int teste1[3] = {10, 15, 20};
+    valorFinal = minCostClimbingStairs(teste1, 3);
 
     // int teste1[10] = {1,100,1,1,1,100,1,1,100,1};
-    // minCostClimbingStairs(teste1, 10);
+    // valorFinal = minCostClimbingStairs(teste1, 10);
 
-    int teste1[4] = {0, 1, 2, 2};
-    valorFinal = minCostClimbingStairs(teste1, 4);
+    // int teste1[4] = {0, 1, 2, 2};
+    // valorFinal = minCostClimbingStairs(teste1, 4);
 
     printf("\n Valor Final: %d", valorFinal);
     return 0;
@@ -20,72 +25,35 @@ int main(){
 
 
 int minCostClimbingStairs(int* cost, int costSize){
-    int finalValue = 0, tempValue1, tempValue2, funcResult, funcResult2;
-    int i, j;
-    
-    for(i = 0, j= 1; i < costSize;){
-        if(i == 0){
-            tempValue1 = cost[i];
-            tempValue2 = cost[j];
+    int i, funcReturn;
 
-            funcResult = encontraMenor(cost, costSize, i);
-            funcResult2 = encontraMenor(cost, costSize, j);
-            
-            if(funcResult == -1){
-                i+=1;
-                finalValue = tempValue1;
-                break;
-            }
-            else
-                tempValue1 += cost[funcResult];
-                
-                
-            if(funcResult2 == -1){
-                i += 2;
-                finalValue = tempValue2;
-                break;
-            }
-            else
-                tempValue2 += cost[funcResult2];
-                
+    for(i = (costSize-1); i >= 0; i--){
+        funcReturn = nextJump(cost, costSize, i);
 
-            if(tempValue1 < tempValue2){
-                i = funcResult;
-                finalValue += cost[0];
-            }
-            else{
-                i = funcResult2;
-                finalValue += cost[1];
-            }
-
-        }
-        else{
-            finalValue += cost[i];
-            funcResult = encontraMenor(cost, costSize, i);
-            if(funcResult == -1)
-                break;
-            i += funcResult;
-        }
+        if(funcReturn == -1)
+            continue;
+        
+        cost[i] = funcReturn;
     }
-        return finalValue;
-    }
-    
+    if(cost[1] < cost[0])
+        return cost[1];
+    else
+        return cost[0];
+}
 
-int encontraMenor(int* vetor, int tamVetor, int posAtual){
+int nextJump(int* vector, int maxPosition,int currentPosition){
     int first, second;
-    
-    if((posAtual +1 )>= tamVetor){
-        return -1;
-    }
-    if((posAtual +2) >= tamVetor ){
-        return -1;
-    }
 
-    first = vetor[posAtual+1];
-    second = vetor[posAtual+2];
+    if((currentPosition + 1) >= maxPosition)
+        return -1;
+    if((currentPosition + 2) >= maxPosition)
+        return -1;
 
-    if(first < second){
-        return 1;
-    }
-    return 2; 
+    first = vector[currentPosition] + vector[currentPosition + 1];
+    second = vector[currentPosition] + vector[currentPosition + 2];
+
+    if(first < second)
+        return first;
+    else
+        return second;
 }
